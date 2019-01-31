4a4ba4b5684315968a7f1038d26004380e9ccf6a
Trung Dang <trungdang@tecace.com>
2019-01-30 16:14:04 -0800

    added modules and .gitignore file.

---
+++ Foundation/Basics/src/FindLongestWordInDictionary.java
@@ -0,0 +1,19 @@
+/**
+ * <p><b>Problem Statement:</b>
+ * <p>Given a string S and a set of words D, find the longest word in D that is a subsequence of S.</p>
+ * <p>Word W is a subsequence of S if some number of characters, possibly zero, can be deleted from S to form W, without reordering the remaining characters.
+ * <p>* Note: D can appear in any format (list, hash table, prefix tree, etc.)</p>
+ * <p>For example, given the input of S = "abppplee" and D = {"able", "ale", "apple", "bale", "kangaroo"} the correct output would be "apple".</p>
+ * <p>The words "able" and "ale" are both subsequences of S, but they are shorter than "apple".</p>
+ * <p>The word "bale" is not a subsequence of S because even though S has all the right letters, they are not in the right order.</p>
+ * <p>The word "kangaroo" is the longest word in D, but it isn't a subsequence of S.</p>
+ */
+public class FindLongestWordInDictionary {
+
+
+
+    public static void main(String[] args) {
+
+    }
+
+}
